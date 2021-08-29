1ad0c644522a9890ebd406b25b4e8b9130245e72
prakash-pra <prakash287902@gmail.com>
2021-08-30 11:07:21 +1200

    cartButton component completed

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,19 +1,23 @@
 import React, { Fragment } from 'react';
 
-import mealsImage from '../../assets/meals.jpg'
-import classes from './Header.module.css'
+import HeaderCartButton from './HeaderCartButton';
 
+import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
+
+// hearder component
 const Header = (props) => {
-  return;
-  <Fragment>
-    <header className={classes.header}>
-      <h1>ReactMeals</h1>
-      <button>Cart</button>
-    </header>
-    <div className={classes['main-image']}>
-      <img src={mealsImage} alt="meals image"/>
-    </div>
-  </Fragment>;
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt='meals' />
+      </div>
+    </Fragment>
+  );
 };
 
 export default Header;
