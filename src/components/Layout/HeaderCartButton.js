17c602d53f131dc01b433faf0936d0c5f9414665
prakash-pra <prakash287902@gmail.com>
2021-09-07 11:02:44 +1200

    adding context hook in cart component

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,9 +1,16 @@
-import { Fragment } from 'react';
-import CartIcon from '../Cart/CartIcon';
+import { Fragment, useContext } from 'react';
 
+import CartContext from '../store/cart-context';
+import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItems = cartCtx.items.reduce((currentNumber, item) => {
+    return currentNumber + item.amount;
+  }, 0);
+
   return (
     <Fragment>
       <button className={classes.button} onClick={props.onClick}>
@@ -11,7 +18,7 @@ const HeaderCartButton = (props) => {
           <CartIcon />
         </span>
         <span>Your Cart</span>
-        <span className={classes.badge}>3</span>
+        <span className={classes.badge}>{numberOfCartItems}</span>
       </button>
     </Fragment>
   );
